9371c49a55c148e56752720597a3b414b89aac09
AyushJfi <ayush.jain@brine.fi>
2024-03-04 12:59:47 +0530

    update file segregation

--- example/deposits.py
+++ example/deposits.py
@@ -8,6 +8,7 @@ from src.brineconnector import exception  # noqa: E402
 from src.brineconnector import create_user_signature
 from src.brineconnector.bin.signature import get_stark_key_pair_from_signature
 from web3 import Web3, Account
+from web3.middleware.geth_poa import geth_poa_middleware
 
 load_dotenv()
 
@@ -15,6 +16,7 @@ PRIVATE_KEY = os.environ['PRIVATE_KEY']
 ETH_ADDRESS = os.environ['ETH_ADDRESS']
 stark_private_key = os.environ['STARK_PRIVATE_KEY']
 rpc_provider = os.environ['RPC_PROVIDER']
+polygon_rpc_provider = os.environ['POLYGON_RPC_PROVIDER']
 
 # Deposits
 def ethereumDeposit():
@@ -72,3 +74,52 @@ def ethereumDeposit():
         print(e)
 
 # ethereumDeposit()
+
+# Deposits for polygon Network
+def polygonDeposit():
+    try:
+        client = Client('testnet')
+        try:
+            # login to use private endpoints
+            login = client.complete_login(ETH_ADDRESS, PRIVATE_KEY)
+            print('Login Successful')
+        # Error: AuthenticationError | requests.exceptions.HTTPError
+        except exception.AuthenticationError as exc:
+            print(exc)
+        except requests.exceptions.HTTPError as exc:
+            print(exc.response.json())
+
+        user_signature = create_user_signature(PRIVATE_KEY, 'testnet')
+        key_pair = get_stark_key_pair_from_signature(user_signature)
+        stark_public_key = key_pair['stark_public_key']
+        provider = Web3(Web3.HTTPProvider(polygon_rpc_provider))
+        provider.middleware_onion.inject(geth_poa_middleware, layer=0)
+        signer = Account.from_key(PRIVATE_KEY)
+
+        # # deposit with eth private key
+        # deposit_res_with_private_key = client.deposit_from_polygon_network(
+        #     polygon_rpc_provider,
+        #     PRIVATE_KEY,
+        #     'matic',
+        #     0.00001
+        # )
+        # print(deposit_res_with_private_key)
+
+
+        # # approval for unlimited allowance for ERC20 contracts
+        # allowance = client.approve_unlimited_allowance_polygon_network(coin='btc', signer=signer, w3=provider)
+        # print(allowance)
+
+        # deposit with signer
+        deposit_res_with_stark_keys = client.deposit_from_polygon_network_with_signer(
+            signer,
+            provider,
+            'btc',
+            0.0001,
+        )
+        print(deposit_res_with_stark_keys)
+
+    except Exception as e:
+        print(e)
+
+polygonDeposit()
