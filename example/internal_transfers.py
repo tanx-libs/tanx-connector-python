88f491d2a2f4ac8b35b5ae955eafe1170289ff68
AyushJfi <ayush.jain@brine.fi>
2024-03-05 18:29:04 +0530

    add internal transfer client methods

---
+++ example/internal_transfers.py
@@ -0,0 +1,77 @@
+import requests
+import sys
+import os
+from dotenv import load_dotenv
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.brineconnector import Client  # noqa: E402
+from src.brineconnector import exception  # noqa: E402
+from src.brineconnector import create_user_signature
+from src.brineconnector.bin.signature import get_stark_key_pair_from_signature
+from web3 import Web3, Account
+
+load_dotenv()
+
+PRIVATE_KEY = os.environ['PRIVATE_KEY']
+ETH_ADDRESS = os.environ['ETH_ADDRESS']
+ETH_ADDRESS_2 = os.environ['ETH_ADDRESS_2']
+stark_private_key = os.environ['STARK_PRIVATE_KEY']
+rpc_provider = os.environ['RPC_PROVIDER']
+BRINE_ORGANIZATION_KEY = os.environ['BRINE_ORGANIZATION_KEY']
+BRINE_API_KEY = os.environ['BRINE_API_KEY']
+
+def internal_transfers():
+    try:
+        client = Client('testnet')
+        try:
+            # login to use private endpoints
+            login = client.complete_login(ETH_ADDRESS, PRIVATE_KEY)
+            print('Login Successful')
+        # Error: AuthenticationError | requests.exceptions.HTTPError
+        except exception.AuthenticationError as exc:
+            print(exc)
+        except requests.exceptions.HTTPError as exc:
+            print(exc.response.json())
+
+        user_signature = create_user_signature(PRIVATE_KEY, 'testnet')
+        key_pair = get_stark_key_pair_from_signature(user_signature)
+        stark_public_key = key_pair['stark_public_key']
+        provider = Web3(Web3.HTTPProvider(rpc_provider))
+        signer = Account.from_key(PRIVATE_KEY)
+
+        # internal_transfer_response = client.intiate_and_process_internal_transfers(
+        #     key_pair=key_pair,
+        #     organization_key=BRINE_ORGANIZATION_KEY,
+        #     api_key=BRINE_API_KEY,
+        #     currency='usdc',
+        #     amount=1,
+        #     destination_address=ETH_ADDRESS_2,
+        #     client_reference_id=1
+        # )
+        # print(internal_transfer_response)
+
+        internal_trasnfers_list = client.list_internal_transfers({
+            'limit': 10
+        })  # type:ignore
+        print(internal_trasnfers_list)
+
+        if len(internal_trasnfers_list['payload']['internal_transfers']):
+            # get the internal transfer by client ID
+            internal_transfer_by_id = client.get_internal_transfer_by_client_id(client_reference_id=internal_trasnfers_list['payload']['internal_transfers'][0]['client_reference_id'])
+            print(internal_transfer_by_id)
+        
+        # check if user exists by their destination address
+        check_user_res = client.check_internal_transfer_user_exists(
+            organization_key=BRINE_ORGANIZATION_KEY,
+            api_key=BRINE_API_KEY,
+            destination_address=ETH_ADDRESS_2,
+        )
+        print(check_user_res)
+
+
+    # except requests.exceptions.HTTPError as e:
+    #     print(e.response.json())
+
+    except Exception as e:
+        print(e)
+
+internal_transfers()
--- src/brineconnector/client.py
+++ src/brineconnector/client.py
@@ -22,6 +22,9 @@ from .typings import (
     Order,
     TokenType,
     ListDepositParams,
+    InternalTransferInitiateBody,
+    InternalTransferProcessBody,
+    ListInternalTransferParams,
 )
 from web3 import Web3, Account
 from .constants import Config
@@ -339,3 +342,49 @@ class Client:
         signer = Account.from_key(eth_private_key)
         return self.deposit_from_ethereum_network_with_starkKey(signer, provider, f'0x{stark_public_key}', amount, currency)
 
+
+    def initiate_internal_transfer(self, body: InternalTransferInitiateBody):
+        r = self.session.post('/sapi/v1/internal_transfers/v2/initiate/', json=body)
+        return r.json()
+
+    def execute_internal_transfers(self, body: InternalTransferProcessBody):
+        r = self.session.post('/sapi/v1/internal_transfers/v2/process/', json=body)
+        return r.json()
+
+    def intiate_and_process_internal_transfers(self, key_pair: dict, organization_key: str, api_key: str, currency: str, amount: float,
+                                            destination_address: str, client_reference_id: Optional[int]=None):
+        self.get_auth_status()
+        initiate_response = self.initiate_internal_transfer({
+            'organization_key': organization_key,
+            'api_key': api_key,
+            'currency': currency,
+            'amount': amount,
+            'destination_address': destination_address,
+            'client_reference_id': client_reference_id
+        }) # type: ignore
+        signature = sign_internal_tx_msg_hash(key_pair, initiate_response['payload']['msg_hash'])
+        execute_response = self.execute_internal_transfers({
+            'organization_key': organization_key,
+            'api_key': api_key,
+            'signature': signature,
+            'nonce': int(initiate_response['payload']['nonce']),
+            'msg_hash': initiate_response['payload']['msg_hash']
+        })
+        return execute_response
+
+    def list_internal_transfers(self, params: Optional[ListInternalTransferParams]=None):
+        self.get_auth_status()
+        r = self.session.get('/sapi/v1/internal_transfers/v2/', params=params)  # type:ignore
+        return r.json()
+
+    def get_internal_transfer_by_client_id(self, client_reference_id: int):
+        self.get_auth_status()
+        r = self.session.get(f'/sapi/v1/internal_transfers/v2/{client_reference_id}')
+        return r.json()
+
+    def check_internal_transfer_user_exists(self, organization_key: str, api_key: str, destination_address: str):
+        self.get_auth_status()
+        loc = locals()
+        body = params_to_dict(loc)
+        r = self.session.post('/sapi/v1/internal_transfers/v2/check_user_exists/', json=body)
+        return r.json()
