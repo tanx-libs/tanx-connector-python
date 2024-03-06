ad0bc73a6d07ba427b998cb4bc9adc45d76793ee
AyushJfi <ayush.jain@brine.fi>
2024-03-06 12:29:47 +0530

    remove env var from test file

--- tests/test_client.py
+++ tests/test_client.py
@@ -12,15 +12,11 @@ from src.brineconnector.typings import Balance  # noqa: E402
 from src.brineconnector.exception import InvalidAmountError, BalanceTooLowError
 from tests.mock_responses import *
 from web3 import EthereumTesterProvider, Account, Web3
-from dotenv import load_dotenv
 BASE_URL = 'https://api.tanx.fi'
 
-load_dotenv()
-
-PRIVATE_KEY = os.environ['PRIVATE_KEY']
-ETH_ADDRESS = os.environ['ETH_ADDRESS']
-stark_private_key = os.environ['STARK_PRIVATE_KEY']
-rpc_provider = os.environ['RPC_PROVIDER']
+# load_dotenv()
+PRIVATE_KEY = '7d6384d6877be027aa25bd458f2058e3f7ff68347dc583a9baf96f5f97b413a8'
+ETH_ADDRESS = '0x713Cf80b7c71440E7a09Dede1ee23dCBf862fB66'
 
 client = Client()
 client2 = Client()
@@ -332,6 +328,7 @@ def test_create_complete_order():
                    status=200)
     nonce_res = client.create_order_nonce({'market': 'btcusdt', 'ord_type': 'market',
                                            'price': 29580.51, 'side': 'buy', 'volume': 0.0001})
+    stark_private_key = '0x64004f706c1eaa39348afb3191c74812d86e5b14b967e578addb4d89ce1234c'
     # replace with your stark private key
     msg_hash = sign_order_with_stark_private_key(
         stark_private_key, nonce_res['payload'])
@@ -348,6 +345,7 @@ def test_create_order_nonce_raises_400_error():
     with pytest.raises(requests.exceptions.HTTPError):
         nonce_res = client.create_order_nonce({'market': 'btcusdt', 'ord_type': 'market',
                                                'price': 29580.51, 'side': 'buy', 'volume': 0.00001})
+        stark_private_key = '0x64004f706c1eaa39348afb3191c74812d86e5b14b967e578addb4d89ce1234c'
         # replace with your stark private key
         msg_hash = sign_order_with_stark_private_key(
             stark_private_key, nonce_res['payload'])
