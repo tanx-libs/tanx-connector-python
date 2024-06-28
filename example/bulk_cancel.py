2c256a599b6fb432916219e2d40074221b5f1aef
Harsh-fi <harsh.jadon@brine.fi>
2024-06-28 11:38:54 +0530

    update: readme.md and bulk cancel example

--- example/bulk_cancel.py
+++ example/bulk_cancel.py
@@ -1,10 +1,13 @@
 import os
 import requests
 import sys
+from dotenv import load_dotenv
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.tanxconnector import Client
 from src.tanxconnector import exception  # noqa: E402
+
+load_dotenv()
 
 PRIVATE_KEY = os.environ['PRIVATE_KEY']
 ETH_ADDRESS = os.environ['ETH_ADDRESS']
